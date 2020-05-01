6e3f92c6a08fed60f9e2e822079b185071138d45
arifeen mahmud <arifeen.mahmud@yahoo.com>
2020-05-02 02:37:32 +0600

    php practice

---
+++ Index.php
@@ -0,0 +1,27 @@
+ <html>
+ <head>
+ <title>My Website</title>
+ </head>
+ <body>
+     <h1>My Website H1 Title ! </h1>
+<!--<?php echo 'Hello World!'; ?> -->
+
+     <?php
+     $name = "Roman. ";
+     echo "My name is  $name";   
+     echo "<p> hello" .$name . "</p>"?>
+     
+     <p> <?php echo 'Hello $name This is the only Text'; ?> </p>
+     <?php echo "Hello $name This is the only Text"; ?>
+
+<p><?php  echo "Hello " . $name; ?></P>
+
+    <p><?php echo "hello $name" ?></P> 
+
+ 
+
+
+
+ </body> 
+   
+ </html>
